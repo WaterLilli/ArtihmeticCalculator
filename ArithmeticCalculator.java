04a8c130aced6b6c4a5d70b97ade3a76092879e3
Lilli <lilli.scheucher@gmail.com>
2022-02-09 16:17:49 +0000

    First commit

--- ArithmeticCalculator.java
+++ ArithmeticCalculator.java
@@ -4,44 +4,44 @@ public class ArithmeticCalculator {
 
 	public static void main(String args¢[]) {
 		
-		double operand1 = 0;
-		double operand2 = 0;
+		double num1 = 0;
+		double num2 = 0;
 		double result = 0;
 		byte choice = 0;
 		
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("ARITHMETIC CALCULATOR");
-		System.out.println("Press 1 to ADD");
-		System.out.println("Press 2 to SUBTRACT");
-		System.out.println("Press 3 to MULTIPLY");
-		System.out.println("Press 4 to DIVIDE");
+		System.out.println("select 1 for +");
+		System.out.println("select 2 for -");
+		System.out.println("select 3 for *");
+		System.out.println("select 4 for /");
 		choice = scan.nextByte();
 		
 		System.out.println("Enter first value: ");
-		operand1 = scan.nextDouble();
+		num1 = scan.nextDouble();
 		
 		System.out.println("Enter second value: ");
-		operand2 = scan.nextDouble();
+		num2 = scan.nextDouble();
 		
 		if(choice == 1) {
 			
-			result = operand1 + operand2;
+			result = num1 + num2;
 			System.out.println("The sum is: " + result);
 		}
 		else if(choice == 2)
 		{
-			result = operand1 - operand2;
+			result = num1 - num2;
 			System.out.println("The difference is: " + result);
 		}
 		else if(choice == 3)
 		{
-			result = operand1 * operand2;
+			result = num1 * num2;
 			System.out.println("The product is: " + result);
 		}
 		else if(choice == 4)
 		{
-			result = operand1 / operand2;
+			result = num1 / num2;
 			System.out.println("The quotient is: " + result);
 			
 		}
